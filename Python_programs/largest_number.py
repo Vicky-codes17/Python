ceff077fa5fd95624c8fcdb35d2258620dabd2e7
Vicky-codes17 <rameshvignesh0706@gmail.com>
2024-10-26 18:23:38 +0530

    largest_number.py

--- Python_programs/largest_number.py
+++ Python_programs/largest_number.py
@@ -6,9 +6,9 @@ num2 = float(input("Enter the second number : "))
 num3 = float(input("Enter the third number : "))
 
 #Finding the largest number
-if num1>=num2 and num1>=num3:
+if num1 >= num2 and num1 >= num3:
     largest = num1
-elif num2>=num1 and num2>=num3:
+elif num2 >= num1 and num2 >= num3:
     largest = num2
 else:
     largest = num3
