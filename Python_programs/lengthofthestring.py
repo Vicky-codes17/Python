fb227b2fd36e1590e7ab91a678b6f235b567e421
Vicky-codes17 <rameshvignesh0706@gmail.com>
2024-10-31 20:07:21 +0530

    lengthofthestring.py

---
+++ Python_programs/lengthofthestring.py
@@ -0,0 +1,9 @@
+    #input_string
+input_string = "Hello World!"
+    #Initialize length counter
+length = 0
+    #For Loop
+for char in input_string:
+    length +=1
+    #Displaying the output
+print("The length of the string is : ",length)
