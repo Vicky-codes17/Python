70606ae2ce3e3f983cc30134be405aac10fe5adf
Vicky-codes17 <rameshvignesh0706@gmail.com>
2024-10-31 20:11:28 +0530

    checksubstring.py

---
+++ Python_programs/checksubstring.py
@@ -0,0 +1,9 @@
+#input string and substring
+main_string = "Hello,Welcome to the worldd of python!"
+
+substring = "python"
+#check if the substring is present in the mainstring
+if substring in main_string:
+    print("The Substring is present  in the string")
+else:
+    print("The Substring is not present in the string")
