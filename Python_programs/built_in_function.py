99d54c2398abf740413742e2be81446c366771cb
Vicky-codes17 <rameshvignesh0706@gmail.com>
2024-11-03 15:29:45 +0530

    built_in_function.py

---
+++ Python_programs/built_in_function.py
@@ -0,0 +1,26 @@
+#This program is about the 5 built-in function in python
+
+#Initial List
+my_list = [5,3,8,6,7]
+
+#1.len()
+list_length = len(my_list)
+print("Lenght of the list  : ",list_length)
+
+#2.max()
+max_value = max(my_list)
+print("Maximum value of the list  : ",max_value)
+
+#3.min()
+min_value = min(my_list)
+print("Minimum value of the list  : ",min_value)
+
+#4.sum()
+total_sum = sum(my_list)
+print("Sum of all elements of the list  : ",total_sum)
+
+#5.sorted()
+sorted_order = sorted(my_list)
+print("Listing after sorting : ",sorted_order)
+
+
