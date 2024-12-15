01fb3864f47fe11adb973d99d237d9f5179a6b36
Vicky-codes17 <rameshvignesh0706@gmail.com>
2024-12-15 08:38:41 +0530

    temperatureconverter.py

---
+++ Python_programs/temperatureconverter.py
@@ -0,0 +1,9 @@
+#Temperature Converter Fahreheit to Celsisus
+
+fahreheit = float(input("Enter the temperature in Fahreheit: "))
+
+#Calculating the temperature in Celsius
+celsius = (fahreheit - 32) / 1.8
+
+#Display the output
+print("The temparature in celsisus :",celsius)
