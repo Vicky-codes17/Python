fe4fa56f003121501e298bd038c9a8b28ea3d1dd
Vicky-codes17 <rameshvignesh0706@gmail.com>
2025-03-12 10:22:45 +0530

    modified functions_addition.py

--- Python_programs/functions_addition.py
+++ Python_programs/functions_addition.py
@@ -1,3 +1,3 @@
-def addition(a,b):
-    print(f"The value of {a} and {b} is {a+b}")
-addition(1,2)
+def addition(a,b): # function definition
+    print(f"The value of {a} and {b} is {a+b}")     
+addition(1,2)   #function call
