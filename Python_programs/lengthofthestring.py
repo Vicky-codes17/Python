10c4409fb4721958c8cd996099f8761aa64c1e68
Vignesh R <153487240+Vicky-codes17@users.noreply.github.com>
2025-12-17 11:17:06 +0530

    Update lengthofthestring.py

--- Python_programs/lengthofthestring.py
+++ Python_programs/lengthofthestring.py
@@ -1,9 +1,8 @@
-    #input_string
+#input_string
 input_string = "Hello World!"
-    #Initialize length counter
 length = 0
-    #For Loop
+#For Loop
 for char in input_string:
     length +=1
-    #Displaying the output
+#Displaying the output
 print("The length of the string is : ",length)
