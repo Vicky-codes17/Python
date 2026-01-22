6ab3bdfb0cf3a33b51db10c01dbba65cc0f8e9ba
Vicky-codes17 <rameshvignesh0706@gmail.com>
2026-01-22 12:31:47 +0530

    Modified: prime_number.py

--- Python_programs/prime_number.py
+++ Python_programs/prime_number.py
@@ -1,18 +1,14 @@
-#Getting the start and end of the interval
 start = int(input("Enter the start of the interval :"))
 end   = int(input("Enter the end of the interval :"))
 
 print(f"Prime numbers between {start} and {end} are : ")
 
-#For Loop 
 for num in range(start,end + 1):
     if num > 1:
         is_prime = True
-    #check if the number has any divisors
         for i in range(2,num):
             if num%i==0:
                 is_prime = False
             break
-        #If no divisors were found,the num is prime
         if is_prime:
             print(num)
